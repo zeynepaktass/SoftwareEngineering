4dc0705e2eecd8512d543b9c3ed0a0cdb5ff1527
Beyza Sezer <81962855+beyzaszr@users.noreply.github.com>
2024-03-24 18:05:16 +0300

    Update c1.js

--- SoftwareEngineering/Scripts/c1.js
+++ SoftwareEngineering/Scripts/c1.js
@@ -8,18 +8,4 @@ updateProgress(targetDonation, currentDonation, 'c1'); // c1 için
 
 
 
-function redirectToComp1() {
-    window.location.href = "comp1.html"; // comp1.html sayfasına yönlendir
-}
 
-function redirectToComp2() {
-    window.location.href = "comp2.html"; // comp2.html sayfasına yönlendir
-}
-
-function redirectToComp3() {
-    window.location.href = "comp3.html"; // comp3.html sayfasına yönlendir
-}
-
-function redirectToComp4() {
-    window.location.href = "comp4.html"; // comp4.html sayfasına yönlendir
-}
