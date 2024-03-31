a6f187294781ba8836ea5d401cf9c1134e9ba463
Beyza Sezer <81962855+beyzaszr@users.noreply.github.com>
2024-03-31 17:43:52 +0300

    Create c1.js

---
+++ Scripts/c1.js
@@ -0,0 +1,11 @@
+
+//veri çekme işlemi bu noktada gerçekleşecek
+targetDonation=100;
+currentDonation=80;
+
+//updateProgress(targetDonation,currentDonation,progressElementId); example of usage
+
+updateProgress(targetDonation, currentDonation, 'c1'); // c1 için
+
+
+
